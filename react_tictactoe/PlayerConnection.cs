890f493ff8cecfc2d2f27291503940c5148e0d92
AdamLeymeister <83959267+AdamLeymeister@users.noreply.github.com>
2022-12-03 20:23:54 -0500

    Delete PlayerConnection.cs

--- react_tictactoe/PlayerConnection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sabio.Web.Api.Hubs.TicTacToe
-{
-    public class PlayerConnection
-    {
-        public string User { get; set; }
-        public string Room { get; set; }
-        public bool IsTurn { get; set; }
-    }
-}
